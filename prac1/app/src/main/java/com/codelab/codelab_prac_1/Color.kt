bbe5fada7a7d9442f843497e3de45173d65a887d
minjong kim <alswhddkdl@gmail.com>
2023-12-21 22:57:59 +0900

    [prac-1] feat: setting color

---
+++ prac1/app/src/main/java/com/codelab/codelab_prac_1/Color.kt
@@ -0,0 +1,8 @@
+package com.codelab.codelab_prac_1
+
+import androidx.compose.ui.graphics.Color
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
