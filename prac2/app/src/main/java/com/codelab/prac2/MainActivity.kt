cdea88f8baea61815651655c90d2a9530e144134
minjong kim <alswhddkdl@gmail.com>
2023-12-28 21:07:23 +0900

    [prac-2] feat: make TextField (with attributes of textfield)

--- prac2/app/src/main/java/com/codelab/prac2/MainActivity.kt
+++ prac2/app/src/main/java/com/codelab/prac2/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.prac2.ui.theme.Prac2Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +41,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+// 검색 가능한 화면(컴포저블)을 생성 -- EditText in xml
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
@@ -40,7 +50,38 @@ fun SearchBar(
         value = "",
         // 값이 바뀔 때 호출되는 콜백
         onValueChange = {},
+        // modifier를 통해 SearchBar의 모양을 바꿔볼 수 있음
+        // 컴포저블의 크기, 레이아웃, 동작, 모양 변경
+        // 접근성 라벨과 같은 정보 추가
+        // 사용자 입력 처리
+        // 요소를 클릭 가능, 스크롤 가능, 드래그 가능 또는 확대/축소 가능하게 만드는 것과 같은 높은 수준의 상호작용 추가
+
+        // 최소 56dp의 높이, 가로는 상위요소의 전체 가로공간으로 설정
         modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp),
+
+        // EditText의 왼쪽에 나타나는 아이콘을 출력
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                // TextField의 placeholder가 이미 텍스트 필드의 의미를 설명하므로 이 아이콘에는 콘텐츠 설명이 필요하지 않습니다.
+                // contentDescription 은 주로 접근성을 위해 사용된다.
+                contentDescription = null
+            )
+        },
+
+        // TextFieldDefaults 의 textFieldColors를 통해 특정 색상을 재정의
+        colors = TextFieldDefaults.textFieldColors(
+            unfocusedSupportingTextColor = MaterialTheme.colorScheme.surface,
+            focusedSupportingTextColor = MaterialTheme.colorScheme.surface
+        ),
+
+
+        // placeholder 지정 (미리보기)
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
     )
 }
 
