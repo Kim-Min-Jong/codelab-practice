a3fba1304831227816821a296d746b66c907da7a
minjong kim <alswhddkdl@gmail.com>
2024-01-01 23:26:38 +0900

    [prac3] feat: make basic text composable

--- prac3/app/src/main/java/com/codelab/prac3/MainActivity.kt
+++ prac3/app/src/main/java/com/codelab/prac3/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.prac3.ui.theme.Prac3Theme
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +29,16 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+// 하루 동안 마신 물잔 개수를 계산하는 워터 카운터 컴포저블
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
+    )
 }
 
 @Composable
