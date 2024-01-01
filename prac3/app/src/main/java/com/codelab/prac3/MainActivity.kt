eb5ab79faa08c971f78607c01b7dffb795a07a8b
minjong kim <alswhddkdl@gmail.com>
2024-01-01 23:32:13 +0900

    [prac3] docs: what is the STATE?

--- prac3/app/src/main/java/com/codelab/prac3/MainActivity.kt
+++ prac3/app/src/main/java/com/codelab/prac3/MainActivity.kt
@@ -51,6 +51,16 @@ fun WellnessScreen(
     WaterCounter(modifier)
 }
 
+/**
+ * 상태란??
+ * 시간이 지남에 따라 변하는 값
+ *
+ * 상태가 업데이트되는 이유는 무엇일까요? Android 앱에서는 이벤트에 대한 응답으로 상태가 업데이트
+ * -> 이벤트는 애플리케이션 외부 또는 내부에서 생성되는 입력 ex) 버튼 누르기 등으로 UI와 상호작용하는 사용자, 기타 요인(예: 새 값을 전송하는 센서 또는 네트워크 응답)
+ *
+ * 핵심 아이디어: 상태는 **존재**하고 이벤트는 **발생**한다.
+ */
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
