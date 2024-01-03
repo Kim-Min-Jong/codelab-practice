f5e1c3fb991089e564faf315e4043c5e0451922d
minjong kim <alswhddkdl@gmail.com>
2024-01-03 21:20:17 +0900

    [prac4] docs: set title this project

--- prac4/app/src/main/java/com/codelab/prac4/MainActivity.kt
+++ prac4/app/src/main/java/com/codelab/prac4/MainActivity.kt
@@ -14,6 +14,10 @@ import com.codelab.prac4.ui.theme.Prac4Theme
 
 /*
     Jetpack Compose의 고급 상태 및 부작용
+
+    Jetpack Compose의 상태 및 부작용 API와 관련된 고급 개념을 알아봅니다.
+    로직이 자명하지 않은 스테이트풀(Stateful) 컴포저블의 상태 홀더를 만드는 방법,
+    Compose 코드에서 코루틴을 만들고 정지 함수를 호출하는 방법, 다양한 사용 사례를 달성하기 위해 부작용을 트리거하는 방법을 살펴보겠습니다.
  */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,25 +29,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Prac4Theme {
-        Greeting("Android")
+
     }
 }
