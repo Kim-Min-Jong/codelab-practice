affc42e8661f5846bc3193dccaf149405ccdd183
minjong kim <alswhddkdl@gmail.com>
2024-01-04 22:53:14 +0900

    [prac4] feat: new value for Main State

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/MainViewModel.kt
@@ -26,6 +26,9 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlin.random.Random
 
 const val MAX_PEOPLE = 4
@@ -38,6 +41,19 @@ class MainViewModel @Inject constructor(
 
     val hotels: List<ExploreModel> = destinationsRepository.hotels
     val restaurants: List<ExploreModel> = destinationsRepository.restaurants
+
+    //  앱을 실행할 때 확인할 수 있듯이 항공편 목적지 목록이 비어 있다.
+    // 다음 두 단계를 완료하여 이 문제를 해결할 수 있습니다.
+    //
+    // ViewModel에 로직을 추가하여 UI 상태를 생성. 이 경우 UI 상태는 추천 목적지 목록.
+    // UI에서 UI 상태를 사용하면 화면에 UI가 표시.
+    // UI 상태를 생성하는 데 사용할 수 있는 API가 몇 가지 있다.
+    // 이 방법 외에 다른 방법은 상태 프로덕션 파이프라인의 출력 유형 문서에 요약되어 있다.
+    // 일반적으로 Kotlin의 StateFlow를 사용하여 UI 상태를 생성하는 것이 좋습니다.
+
+    // 변수 캡슐화
+    private val _suggestedDestination = MutableStateFlow<List<ExploreModel>>(emptyList())
+    val suggestedDestinations: StateFlow<List<ExploreModel>> = _suggestedDestination.asStateFlow()
 
     fun updatePeople(people: Int) {
         viewModelScope.launch {
