2ec5e465504f2efc3b888122f36013bb1a5027b3
minjong kim <alswhddkdl@gmail.com>
2024-01-04 16:58:09 +0900

    [prac4] docs: set title this project

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/CraneApplication.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/CraneApplication.kt
@@ -22,6 +22,15 @@ import coil.ImageLoader
 import coil.ImageLoaderFactory
 import dagger.hilt.android.HiltAndroidApp
 
+/*
+    Jetpack Compose의 고급 상태 및 부작용
+
+    Jetpack Compose의 상태 및 부작용 API와 관련된 고급 개념을 알아봅니다.
+    로직이 자명하지 않은 스테이트풀(Stateful) 컴포저블의 상태 홀더를 만드는 방법,
+    Compose 코드에서 코루틴을 만들고 정지 함수를 호출하는 방법,
+    다양한 사용 사례를 달성하기 위해 부작용을 트리거하는 방법을 살펴보겠습니다.
+ */
+
 @HiltAndroidApp
 class CraneApplication : Application(), ImageLoaderFactory {
 
