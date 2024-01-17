112016939b32bee012bb9b92ff70048418111597
minjong kim <alswhddkdl@gmail.com>
2024-01-17 16:43:01 +0900

    [prac6] feat: set the viewModel

---
+++ room&flow/app/src/main/java/com/example/busschedule/BusScheduleViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.busschedule
+
+import androidx.lifecycle.ViewModel
+
+// 데이터베이스에 접근할 뷰모델 정의
+
+// ViewModel 클래스는 앱 UI 관련 데이터를 저장하는 데 사용되고 수명 주기도 인식하므로 활동이나 프래그먼트와 마찬가지로 수명 주기 이벤트에 응답
+// 화면 회전과 같은 수명 주기 이벤트로 인해 활동이나 프래그먼트가 소멸되었다가 다시 생성되면 연결된 ViewModel을 다시 만들 필요가 없다.
+// DAO 에 직접 액세스할 때는 불가능하므로 ViewModel 서브클래스를 사용하여 활동이나 프래그먼트에서 데이터를 로드하는 책임을 분리하는 것이 좋다.
+class BusScheduleViewModel(
+    private val scheduleDao: ScheduleDao
+): ViewModel() {
+
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+
+}
---
+++ room&flow/app/src/main/java/com/example/busschedule/BusScheduleViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.example.busschedule
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+// 뷰모델은 직접 인스턴스화를 할 수 없어서 다른 방법을 사용해야한다.
+// ViewModel 클래스 BusScheduleViewModel은 수명 주기를 인식해야 하므로 수명 주기 이벤트에 응답할 수 있는 객체로 인스턴스화해야한다.
+// 프래그먼트 중 하나에서 직접 인스턴스화하면 프래그먼트 객체가 앱 코드의 기능 범위를 벗어나는 모든 메모리 관리 등 모든 것을 처리해야 한다.
+// 대신 뷰 모델 객체를 인스턴스화하는 팩토리라는 클래스를 만들 수 있다.
+
+// activity-ktx, fragment-ktx 등 라이브러리로 위임자를 통해 팩토리 선언을 하지 않고도 사용할 수 있다.
+class BusScheduleViewModelFactory(
+    private val scheduleDao: ScheduleDao
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return BusScheduleViewModel(scheduleDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
