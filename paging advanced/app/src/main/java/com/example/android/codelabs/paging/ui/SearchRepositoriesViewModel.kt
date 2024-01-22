7e7f03d794015b3499308eca11290aeedcca8156
minjong kim <alswhddkdl@gmail.com>
2024-01-22 22:48:45 +0900

    [prac8] fix: fix the viewModel with PagingData - 3 (define the UiAction)

--- paging advanced/app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
+++ paging advanced/app/src/main/java/com/example/android/codelabs/paging/ui/SearchRepositoriesViewModel.kt
@@ -25,10 +25,20 @@ import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.liveData
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import com.example.android.codelabs.paging.data.GithubRepository
+import com.example.android.codelabs.paging.model.Repo
 import com.example.android.codelabs.paging.model.RepoSearchResult
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 
 /**
@@ -47,64 +57,84 @@ class SearchRepositoriesViewModel(
     // 참고: PagingData Flow를 다른 Flows와 함께 사용하거나 결합X
     // 내보낸 PagingData는 각각 독립적으로 사용되어야한다.
     // 또한 shareIn 및 stateIn 같은 연산자를 PagingData Flows에 사용해서는 안 된다.
-    val state: LiveData<UiState>
+    val state: StateFlow<UiState>
+
+    val pagingDataFlow: Flow<PagingData<Repo>>
+
 
     /**
      * Processor of side effects from the UI which in turn feedback into [state]
      */
+    // 뷰모델의 요청에 대한 하나의 접근 점(진입점)
+    // 다음과 같은 동작을 수행해야함
+    // 1. 진입점을 관심 있는 유형이 포함된 스트림으로 변환
+    // 2. 그러한 스트림을 변환합니다.
+    // 3. 스트림을 다시 StateFlow<UiState>로 결합
     val accept: (UiAction) -> Unit
 
     init {
         val queryLiveData =
             MutableLiveData(savedStateHandle.get(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY)
 
-        state = queryLiveData
-            .distinctUntilChanged()
-            .switchMap { queryString ->
-                liveData {
-                    val uiState = repository.getSearchResultStream(queryString)
-                        .map {
-                            UiState(
-                                query = queryString,
-                                searchResult = it
-                            )
-                        }
-                        .asLiveData(Dispatchers.Main)
-                    emitSource(uiState)
-                }
-            }
+        // ViewModel의  accept를 UiAction으로 변환하는 작업
 
-        accept = { action ->
-            when (action) {
-                is UiAction.Search -> queryLiveData.postValue(action.query)
-                is UiAction.Scroll -> if (action.shouldFetchMore) {
-                    val immutableQuery = queryLiveData.value
-                    if (immutableQuery != null) {
-                        viewModelScope.launch {
-                            repository.requestMore(immutableQuery)
-                        }
-                    }
-                }
+        // 초기 쿼리 (저장된 쿼리가 있으면 그것으로 설정 없으면 디폴트값)
+        val initialQuery: String = savedStateHandle.get(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY
+        // 마지막으로 스크롤시 나왔던 쿼리 (사용자가 목록과 상호작용한 마지막 검색어)
+        val lastQueryScrolled: String = savedStateHandle.get(LAST_QUERY_SCROLLED) ?: DEFAULT_QUERY
+        // 데이터 상태 스트림
+        val actionStateFlow = MutableStateFlow<UiAction>()
+
+        // Flow를 특정 UiAction 유형으로 분할
+        // UiAction.Search - 사용자가 특정 쿼리를 입력하는 각 경우
+        // UiAction.Scroll - 사용자가 포커스가 지정된 특정 쿼리와 함께 목록을 스크롤하는 각 경우
+        // 1
+        val searches = actionStateFlow
+            .filterIsInstance<UiAction.Search>()
+            .distinctUntilChanged()
+            .onStart {
+                emit(UiAction.Search(query =  initialQuery))
             }
-        }
+        // 2
+        val queriesScrolled = actionStateFlow
+            .filterIsInstance<UiAction.Scroll>()
+            .distinctUntilChanged()
+            // Flow가 최종적으로 사용될 떄, flatmaplatest 연산자를 사용되기 떄문에 필요
+            // 업스트림에서 내보낼 때 마다, flatmaplatest는 마지막으로 작업한 flow를 취소하고, 새 스트림을 기반으로 작업을 시작하는데,
+            // 여기서 사용자가 스크롤한 마지막 쿼리의 값이 손실됨
+            // 따라서 새 쿼리가 수신되더라도 값이 손실되지 않도록 replay = 1인 Flow를 사용하여 마지막 값을 캐시
+            .shareIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5000),
+                replay = 1
+            )
+            .onStart { emit(UiAction.Scroll(currentQuery = lastQueryScrolled)) }
+
     }
 
     override fun onCleared() {
-        savedStateHandle[LAST_SEARCH_QUERY] = state.value?.query
+        savedStateHandle[LAST_SEARCH_QUERY] = state.value.query
+        // 스크롤도 복원되애하는 항목이므로 save
+        savedStateHandle[LAST_QUERY_SCROLLED] = state.value.lastQueryScrolled
         super.onCleared()
     }
-}
 
-private val UiAction.Scroll.shouldFetchMore
-    get() = visibleItemCount + lastVisibleItemPosition + VISIBLE_THRESHOLD >= totalItemCount
+    private fun searchRepo(queryString: String): Flow<PagingData<Repo>> =
+        repository.getSearchResultStream(queryString)
+}
+// This is outside the ViewModel class, but in the same file
+private const val LAST_QUERY_SCROLLED: String = "last_query_scrolled"
+
+// 삭제
+//private val UiAction.Scroll.shouldFetchMore
+//    get() = visibleItemCount + lastVisibleItemPosition + VISIBLE_THRESHOLD >= totalItemCount
 
 // 상태 관리를 위한 sealed class
 sealed class UiAction {
     data class Search(val query: String) : UiAction()
     data class Scroll(
-        val visibleItemCount: Int,
-        val lastVisibleItemPosition: Int,
-        val totalItemCount: Int
+        // 스크롤 작업을 특정 쿼리와 연결 시키기 위한 프로퍼티 설정
+        val currentQuery: String
     ) : UiAction()
 }
 
