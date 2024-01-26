d253b5b0dc4028381413b95c549f3c4c72cd53b3
minjong kim <alswhddkdl@gmail.com>
2024-01-26 16:57:10 +0900

    [prac9] feat: fake storage

---
+++ android-dagger/app/src/main/java/com/example/android/dagger/storage/FakeStorage.kt
@@ -0,0 +1,11 @@
+package com.example.android.dagger.storage
+
+class FakeStorage: Storage {
+    override fun setString(key: String, value: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getString(key: String): String {
+        TODO("Not yet implemented")
+    }
+}
