34fff6b74182336ff2c0f334b449b2081814b8b3
minjong kim <alswhddkdl@gmail.com>
2024-02-15 15:36:24 +0900

    [prac10][compose with viewModel] feat: mix the word

--- compose with ViewModel/app/src/main/java/com/example/unscramble/GameViewModel.kt
+++ compose with ViewModel/app/src/main/java/com/example/unscramble/GameViewModel.kt
@@ -1,11 +1,17 @@
 package com.example.unscramble
 
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class GameViewModel: ViewModel() {
+
+    init {
+        // 뷰모델 초기화시 한번 단어와 상태를 초기화
+        resetGame()
+    }
 
     // StateFlow
     // 현재 상태와 새로운 상태를 확인해 내보내는 옵저버블한 데이터 홀더 flow
@@ -24,4 +30,38 @@ class GameViewModel: ViewModel() {
     // UI에 보여지기만 할 변수 - UI 단에서는 수정 불가능 (val)
     val count: Int
         get() = _count
+
+    // 현재 단어 변수
+    private lateinit var currentWord: String
+
+    // 사용된 단어를 저장하는 집합
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    // 임의 단어를 선택하는 함수
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        return if (usedWords.contains(currentWord)) {
+            pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            shuffleCurrentWord(currentWord)
+        }
+    }
+
+    // 임의 단어의 철자들을 섞는 함수
+    private fun shuffleCurrentWord(word: String): String {
+        val tmpWord = word.toCharArray()
+        tmpWord.shuffle()
+        // 섞었을 때도 같으면 다를 떄 까지 계속 섞기
+        while(String(tmpWord) == word) {
+            tmpWord.shuffle()
+        }
+        return String(tmpWord)
+    }
+
+    // 초기화
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
 }
