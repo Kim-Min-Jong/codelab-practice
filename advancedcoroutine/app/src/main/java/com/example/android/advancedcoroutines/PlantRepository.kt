7d6358d2d4322c9d2cd6ac92016d51add5ac2c77
minjong kim <alswhddkdl@gmail.com>
2024-02-21 17:09:10 +0900

    [prac10][advanced coroutine] feat: combining flows declaratively

--- advancedcoroutine/app/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ advancedcoroutine/app/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -24,7 +24,14 @@ import com.example.android.advancedcoroutines.utils.CacheOnSuccess
 import com.example.android.advancedcoroutines.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.switchMap
 import kotlinx.coroutines.withContext
 
@@ -61,6 +68,21 @@ class PlantRepository private constructor(
     // livedata -> flow
     val plantsFlow: Flow<List<Plant>>
         get() = plantDao.getPlantsFlow()
+            // 여러 flow를 결합 - combine (여기선 정렬 시키는 flow)
+            // 두 flow는 자체 코루틴에서 실행되고 각 flow에서 새 값이 생성 될 때마다 각 flow의 최신 값으로 변환 되어 호출
+            // 여기서는 네트워크 조회, 데이터베이스 실행과 결합해서 사용하게 됨
+            // 이 두 동작은 다른 코루틴에서 동시 실행 됨
+            // 두 flow 결과가 나오는 즉시 정렬을 적용하게 됨
+            .combine(customSortFlow) { plants, sortOrder ->
+                plants.applySort(sortOrder)
+            }
+            // 데이터 세트의 규모가 커질수록 applySort 호출 시 기본 스레드를 차단할 정도로 속도가 떨어질 수도 있음
+            // flowOn이라는 선언적 API를 제공하여 흐름이 실행되는 스레드를 제어
+            // 이전까지의 흐름을 수집하는 새 코루틴을 실행하고 결과를 작성하는 버퍼를 도입
+            //  변환 도중에 흐름의 작동 방식을 변경하는 버퍼가 도입되어 flowOn에서 실행된 코루틴은 호출자가 소비하는 속도보다 빠르게 결과를 생성할 수 있고 많은 결과를 버퍼링함
+            .flowOn(defaultDispatcher)
+            // 생성된 마지막 값만 버퍼에 저장하도록 지시
+            .conflate()
 
 
     // 정렬 순서를 가져와 메모리에 캐시하는 함수
@@ -70,6 +92,19 @@ class PlantRepository private constructor(
             plantService.customPlantSortOrder()
         }
 
+    // 정렬을 위한 새 flow
+    // collect되면 getOrAwait를 내보내고 정렬 된 리스트를 emit함
+    private val customSortFlow = flow {
+        emit(plantsListSortOrderCache.getOrAwait())
+    }
+
+    // 위 flow는 단일 값만 내보내므로 asFlow를 사용해서 축약해볼 수 있음
+    private val customSortFlowV2 = plantsListSortOrderCache::getOrAwait.asFlow()
+        .onStart {
+            emit(listOf())
+            // 필요에 의하면 지연 시키는 것도 가능
+            delay(1500L)
+        }
     // 리스트를 다시 정렬해 customSortOrder에 있는 Plants를 목록 앞부분에 배치
     private fun List<Plant>.applySort(customOrder: List<String>): List<Plant> =
         sortedBy { plant ->
