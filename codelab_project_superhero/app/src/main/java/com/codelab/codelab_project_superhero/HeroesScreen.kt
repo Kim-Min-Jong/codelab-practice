91faae280c14ee373fc62c9cfafac9b3b527df66
minjong kim <alswhddkdl@gmail.com>
2024-03-04 17:03:07 +0900

    [codelab-project1][super hero] feat: make hero card column composable

--- codelab_project_superhero/app/src/main/java/com/codelab/codelab_project_superhero/HeroesScreen.kt
+++ codelab_project_superhero/app/src/main/java/com/codelab/codelab_project_superhero/HeroesScreen.kt
@@ -3,6 +3,7 @@ package com.codelab.codelab_project_superhero
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,18 +11,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.codelab.codelab_project_superhero.model.Hero
+import com.codelab.codelab_project_superhero.model.HeroesRepository
+import com.codelab.codelab_project_superhero.ui.theme.md_theme_light_onPrimaryContainer
+import com.codelab.codelab_project_superhero.ui.theme.md_theme_light_primary
 
 @Composable
 fun HeroContent(
@@ -31,16 +43,22 @@ fun HeroContent(
     Card(
         // elevation 2dp
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-        modifier = modifier
+        modifier = modifier.padding(
+            PaddingValues(
+                horizontal = 16.dp,
+                vertical = 8.dp
+            ),
+        )
     ) {
         Row(
             modifier = Modifier
                 .fillMaxSize()
                 .sizeIn(72.dp)
-                .padding(16.dp)
+                .padding(16.dp),
         ) {
             Column(
-
+                // 균형 맞추기 (모든 공간 사용)
+                modifier = Modifier.weight(1f)
             ) {
                 Text(
                     text = stringResource(id = hero.nameRes),
@@ -64,5 +82,44 @@ fun HeroContent(
             }
 
         }
+    }
+}
+
+@Composable
+fun MainView(
+    modifier: Modifier = Modifier,
+    list: List<Hero>,
+    contentPadding: PaddingValues
+) {
+    LazyColumn(
+        contentPadding = contentPadding
+    ) {
+        items(list) { hero ->
+            HeroContent(hero = hero)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContentWithTopAppBar() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    titleContentColor = Color.Black,
+                    containerColor = md_theme_light_primary,
+                ),
+                title = {
+                    Text("SuperHeroes")
+                }
+            )
+        },
+    ) { innerPadding ->
+        MainView(
+            list = HeroesRepository.heroes,
+            contentPadding = innerPadding
+        )
     }
 }
