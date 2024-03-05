ad26f7e6dfaa981ab97be06bc6839215d193cd44
minjong kim <alswhddkdl@gmail.com>
2024-03-05 15:27:18 +0900

    [codelab-project2][DiceRoller] feat: build composable units

--- DiceRoller/app/src/main/java/com/codelab/diceroller/DiceScreen.kt
+++ DiceRoller/app/src/main/java/com/codelab/diceroller/DiceScreen.kt
@@ -1,10 +1,19 @@
 package com.codelab.diceroller
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DiceRollerApp() {
@@ -13,8 +22,9 @@ fun DiceRollerApp() {
     // 리컴포지션시 기본 인자가 다시 생성되므로 비효율적이여서 인자로 제공
     DiceWithButtonAndImage(
         // 가득 채우되
-        modifier = Modifier.fillMaxSize()
-        // Alignment를 이용해서 공간내 중앙 정렬
+        modifier = Modifier
+            .fillMaxSize()
+            // Alignment를 이용해서 공간내 중앙 정렬
             .wrapContentSize(Alignment.Center)
     )
 }
@@ -23,5 +33,21 @@ fun DiceRollerApp() {
 fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
 ) {
-
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.dice_1),
+            contentDescription = "1"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                
+            }
+        ) {
+            Text(text = stringResource(id = R.string.roll))
+        }
+    }
 }
