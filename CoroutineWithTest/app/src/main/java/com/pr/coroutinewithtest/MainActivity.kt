e6be678c9844a8bf6dc7847f892ae7b8df7e3b5f
minjong kim <alswhddkdl@gmail.com>
2024-07-18 11:02:47 +0900

    [Coroutine with Test] feat: create basic ui

--- CoroutineWithTest/app/src/main/java/com/pr/coroutinewithtest/MainActivity.kt
+++ CoroutineWithTest/app/src/main/java/com/pr/coroutinewithtest/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.racetracker.ui.RaceTrackerApp
 import com.pr.coroutinewithtest.ui.theme.CoroutineWithTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +20,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CoroutineWithTestTheme {
-                
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                RaceTrackerApp()
             }
         }
     }
---
+++ CoroutineWithTest/app/src/main/java/com/pr/coroutinewithtest/ui/RaceTrackerApp.kt
@@ -0,0 +1,233 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.racetracker.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.pr.coroutinewithtest.R
+import com.pr.coroutinewithtest.ui.RaceParticipant
+import com.pr.coroutinewithtest.ui.progressFactor
+
+@Composable
+fun RaceTrackerApp() {
+    /**
+     * Note: To survive the configuration changes such as screen rotation, [rememberSaveable] should
+     * be used with custom Saver object. But to keep the example simple, and keep focus on
+     * Coroutines that implementation detail is stripped out.
+     */
+    // 플1의 객체
+    val playerOne = remember {
+        RaceParticipant(name = "Player 1", progressIncrement = 1)
+    }
+    // 플2의 객체
+    val playerTwo = remember {
+        RaceParticipant(name = "Player 2", progressIncrement = 2)
+    }
+
+    // 레이스가 시작 되었는지
+    var raceInProgress by remember { mutableStateOf(false) }
+    // 호이스팅을 통한 상태 전달
+    // 레이스 진행률 반영
+    RaceTrackerScreen(
+        playerOne = playerOne,
+        playerTwo = playerTwo,
+        isRunning = raceInProgress,
+        onRunStateChange = { raceInProgress = it },
+        modifier = Modifier
+            .statusBarsPadding()
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding()
+            .padding(horizontal = dimensionResource(R.dimen.padding_medium)),
+    )
+}
+
+@Composable
+private fun RaceTrackerScreen(
+    playerOne: RaceParticipant,
+    playerTwo: RaceParticipant,
+    isRunning: Boolean,
+    onRunStateChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.run_a_race),
+            style = MaterialTheme.typography.headlineSmall,
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(dimensionResource(R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_walk),
+                contentDescription = null,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+            )
+            // 프로그래스바가 포함 된 컴포저블
+            StatusIndicator(
+                participantName = playerOne.name,
+                currentProgress = playerOne.currentProgress,
+                maxProgress = stringResource(
+                    R.string.progress_percentage,
+                    playerOne.maxProgress
+                ),
+                progressFactor = playerOne.progressFactor,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
+            StatusIndicator(
+                participantName = playerTwo.name,
+                currentProgress = playerTwo.currentProgress,
+                maxProgress = stringResource(
+                    R.string.progress_percentage,
+                    playerTwo.maxProgress
+                ),
+                progressFactor = playerTwo.progressFactor,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(modifier = Modifier.size(dimensionResource(R.dimen.padding_large)))
+            RaceControls(
+                isRunning = isRunning,
+                onRunStateChange = onRunStateChange,
+                onReset = {
+                    // 모든 플레이어의 상태를 리셋하고
+                    playerOne.reset()
+                    playerTwo.reset()
+                    // 진행을 정지
+                    onRunStateChange(false)
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
+
+@Composable
+private fun StatusIndicator(
+    participantName: String,
+    currentProgress: Int,
+    maxProgress: String,
+    progressFactor: Float,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+    ) {
+        Text(
+            text = participantName,
+            modifier = Modifier.padding(end = dimensionResource(R.dimen.padding_small))
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            // 진행률 프로그래스 바
+            LinearProgressIndicator(
+                // 리컴포지션이 발생할 때마다 갱신 됨
+                progress = progressFactor,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(dimensionResource(R.dimen.progress_indicator_height))
+                    .clip(RoundedCornerShape(dimensionResource(R.dimen.progress_indicator_corner_radius)))
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = stringResource(R.string.progress_percentage, currentProgress),
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = maxProgress,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
+
+// 레이스 시작, 정지를 제어하는 컴포저블
+@Composable
+private fun RaceControls(
+    onRunStateChange: (Boolean) -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier,
+    isRunning: Boolean = true,
+) {
+    Column(
+        modifier = modifier.padding(top = dimensionResource(R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium))
+    ) {
+        // 시작, 정지 버튼
+        Button(
+            // 클릭 할 때마다 시작 - 정지 변환
+            onClick = { onRunStateChange(!isRunning) },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(if (isRunning) stringResource(R.string.pause) else stringResource(R.string.start))
+        }
+        // 리셋 버튼
+        OutlinedButton(
+            // 진행률을 0으로 만듦
+            onClick = onReset,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(stringResource(R.string.reset))
+        }
+    }
+}
