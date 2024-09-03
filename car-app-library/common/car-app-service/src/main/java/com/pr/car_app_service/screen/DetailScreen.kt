0ea942f8414b700930bfd30083ec1f3321c16610
minjong kim <alswhddkdl@gmail.com>
2024-09-03 13:49:07 +0900

    [car app library] feat: Update the content on a screen

--- car-app-library/common/car-app-service/src/main/java/com/pr/car_app_service/screen/DetailScreen.kt
+++ car-app-library/common/car-app-service/src/main/java/com/pr/car_app_service/screen/DetailScreen.kt
@@ -1,8 +1,11 @@
 package com.pr.car_app_service.screen
 
+import android.graphics.Color
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
+import androidx.car.app.model.ActionStrip
+import androidx.car.app.model.CarColor
 import androidx.car.app.model.CarIcon
 import androidx.car.app.model.MessageTemplate
 import androidx.car.app.model.Pane
@@ -18,6 +21,8 @@ class DetailScreen(
     carContext: CarContext,
     private val placeId: Int
 ) : Screen(carContext) {
+    // 상태 추가
+    private var isFavorite = false
     override fun onGetTemplate(): Template {
         // 장소 가져옴
         val place = PlacesRepository().getPlace(placeId)
@@ -40,6 +45,33 @@ class DetailScreen(
                 carContext.startCarApp(place.toIntent(CarContext.ACTION_NAVIGATE))
             }.build()
 
+        // 이 컴포넌트는 타이틀 반대편 헤더 행에 놓는 것이 좋고 주요액션이 아니여야 한다.
+        // 주요액션이 아니기 때문에 action이 아닌 actionStrip을 통해 스크린에 보여준다.
+        val actionStrip = ActionStrip.Builder()
+            .addAction(
+                Action.Builder()
+                    .setIcon(
+                        CarIcon.Builder(
+                            IconCompat.createWithResource(
+                                carContext,
+                                R.drawable.baseline_favorite_24
+                            )
+                        ).setTint(
+                            if (isFavorite) CarColor.RED else CarColor.createCustom(
+                                Color.LTGRAY,
+                                Color.DKGRAY
+                            )
+
+                        ).build()
+                    )
+                    .setOnClickListener{
+                        // 상태를 바꾼후
+                        isFavorite = !isFavorite
+                        // 다시 그림
+                        invalidate()
+                    }.build()
+            ).build()
+
         return PaneTemplate.Builder(
             Pane.Builder()
                 .addAction(navigationAction)
@@ -56,6 +88,7 @@ class DetailScreen(
                 ).build()
         ).setTitle(place.name)
             .setHeaderAction(Action.BACK)
+            .setActionStrip(actionStrip)
             .build()
     }
 
