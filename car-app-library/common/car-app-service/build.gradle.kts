5f83d146e34469ac4a0cadbc28080267fc955678
minjong kim <alswhddkdl@gmail.com>
2024-09-03 10:45:39 +0900

    [car app library] feat: set up initial configuration

--- car-app-library/common/car-app-service/build.gradle.kts
+++ car-app-library/common/car-app-service/build.gradle.kts
@@ -37,6 +37,11 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+
+    // car app library 코어 클래스 모듈 (app-projected, app-testing, app-automotive)
+    implementation("androidx.car.app:app:1.3.0-rc01")
+    implementation(project(":common:data"))
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
