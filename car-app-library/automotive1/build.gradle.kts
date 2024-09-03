2e8b79d456609329ee734808a2bd37dec8ac3fce
minjong kim <alswhddkdl@gmail.com>
2024-09-03 12:10:15 +0900

    [car app library] feat: Add support for Android Automotive OS 2 (add dependencies)

--- car-app-library/automotive1/build.gradle.kts
+++ car-app-library/automotive1/build.gradle.kts
@@ -39,6 +39,10 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation(project(":common:car-app-service"))
+    implementation("androidx.car.app:app:1.4.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
