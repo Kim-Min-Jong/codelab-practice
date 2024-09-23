92eb542bacf82b9e297a2feb7ab5e84a4a1652d8
minjong kim <alswhddkdl@gmail.com>
2024-09-23 13:04:02 +0900

    [android stylus] feat: make stylusState class

--- android-stylus/app/src/main/java/com/example/stylus/MainActivity.kt
+++ android-stylus/app/src/main/java/com/example/stylus/MainActivity.kt
@@ -30,17 +30,23 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import com.example.stylus.ui.theme.StylusTheme
 import androidx.compose.ui.draw.clipToBounds
+import com.example.stylus.ui.StylusState
 
 class MainActivity : ComponentActivity() {
 
     private val viewModel: StylusViewModel by viewModels()
     private val strokeStyle = Stroke(10F)
+    private var stylusState: StylusState by mutableStateOf(StylusState())
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ android-stylus/app/src/main/java/com/example/stylus/ui/StylusState.kt
@@ -0,0 +1,14 @@
+package com.example.stylus.ui
+
+import androidx.compose.ui.graphics.Path
+
+data class StylusState (
+    // 굵기
+    val pressure: Float = 0f,
+    // 방향 (외부로의)
+    val orientation: Float = 0f,
+    // 방향 (내부로의)
+    val tilt: Float = 0f,
+    // 렌더링된 선을 drawPath 메서드를 사용하여 저장
+    val path: Path = Path(),
+)
