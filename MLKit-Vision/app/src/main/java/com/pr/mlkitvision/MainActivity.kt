844f6b879eda3a0e1bae4636f90562e8a30a89cf
minjong kim <alswhddkdl@gmail.com>
2024-11-19 14:25:42 +0900

    [MLKit Practice (Vision)] feat: initialize view components

--- MLKit-Vision/app/src/main/java/com/pr/mlkitvision/MainActivity.kt
+++ MLKit-Vision/app/src/main/java/com/pr/mlkitvision/MainActivity.kt
@@ -1,20 +1,88 @@
 package com.pr.mlkitvision
 
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.mlkit.vision.common.InputImage
+import java.util.PriorityQueue
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private val TAG: String = "MainActivity"
+    private var mImageView: ImageView? = null
+    private var mTextButton: Button? = null
+    private var mFaceButton: Button? = null
+    private val mSelectedImage: Bitmap? = null
+    private var mGraphicOverlay: GraphicOverlay? = null
+
+    // Max width (portrait mode)
+    private val mImageMaxWidth: Int? = null
+
+    // Max height (portrait mode)
+    private val mImageMaxHeight: Int? = null
+
+    /**
+     * Number of results to show in the UI.
+     */
+    private val RESULTS_TO_SHOW: Int = 3
+
+    /**
+     * Dimensions of inputs.
+     */
+    private val DIM_BATCH_SIZE: Int = 1
+    private val DIM_PIXEL_SIZE: Int = 3
+    private val DIM_IMG_SIZE_X: Int = 224
+    private val DIM_IMG_SIZE_Y: Int = 224
+
+    private val sortedLabels = PriorityQueue<Map.Entry<String, Float>>(
+        RESULTS_TO_SHOW
+    ) { o1, o2 -> o1.value.compareTo(o2.value) }
+
+    /* Preallocated buffers for storing image data. */
+    private val intValues = IntArray(DIM_IMG_SIZE_X * DIM_IMG_SIZE_Y)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        mImageView = findViewById(R.id.image_view)
+
+        mTextButton = findViewById(R.id.button_text)
+        mFaceButton = findViewById(R.id.button_face)
+
+        mGraphicOverlay = findViewById(R.id.graphic_overlay)
+
+
+        mTextButton!!.setOnClickListener { runTextRecognition() }
+        mFaceButton!!.setOnClickListener { runFaceContourDetection() }
+        val dropdown = findViewById<Spinner>(R.id.spinner)
+        val items = arrayOf("Test Image 1 (Text)", "Test Image 2 (Face)")
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item, items
+        )
+        dropdown.adapter = adapter
+        dropdown.onItemSelectedListener = this@MainActivity
+    }
+
+    private fun runFaceContourDetection() {
+
+    }
+
+    private fun runTextRecognition() {
+
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
