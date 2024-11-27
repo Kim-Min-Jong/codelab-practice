38b7ba719160923e631bc29e9147e86a80509f20
minjong kim <alswhddkdl@gmail.com>
2024-11-27 11:56:32 +0900

    [Compose Testing] feat: test TopAppBar (case test)

--- TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
@@ -36,4 +37,24 @@ class TopAppBarTest {
             .assertIsSelected()
     }
 
+
+    @Test
+    fun rallyTopBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.entries
+
+        // 룰을 통해 compose 함수 사용가능
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = {},
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            // 탭의 문자를 대문자로 표시
+            .onNodeWithContentDescription(RallyScreen.Accounts.name.uppercase())
+            // 대문자로 존재하는지 확인
+            .assertExists()
+    }
 }
