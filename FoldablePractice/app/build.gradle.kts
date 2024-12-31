25682a604da4989d0e81164b90506810acf0d693
minjong kim <alswhddkdl@gmail.com>
2024-12-31 15:28:53 +0900

    [Foldable & dual monitor] feat: add dependencies for window manager

--- FoldablePractice/app/build.gradle.kts
+++ FoldablePractice/app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.window)
+    androidTestImplementation(libs.androidx.window.testing)
+    implementation(libs.androidx.lifcycle.runtime)
 }
