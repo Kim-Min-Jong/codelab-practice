7e931ae6b487326933a4b0b3888d7112e585c2a8
minjong kim <alswhddkdl@gmail.com>
2024-12-31 15:30:56 +0900

    [Foldable & dual monitor] feat: access window manager

--- FoldablePractice/app/src/main/java/com/pr/foldablepractice/MainActivity.kt
+++ FoldablePractice/app/src/main/java/com/pr/foldablepractice/MainActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.window.layout.WindowInfoTracker
 
 class MainActivity : AppCompatActivity() {
+    // 윈도우 매니저 변수 선언
+    private lateinit var windowInfoTracker: WindowInfoTracker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // window manager access
+        // 현재 기기의 창 상태의 정보 상태가 담김
+        windowInfoTracker = WindowInfoTracker.getOrCreate(this@MainActivity)
     }
 }
