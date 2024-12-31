063e11dcbb61a1ea98a5a91b3ac1cbc5fbe1be34
minjong kim <alswhddkdl@gmail.com>
2024-12-31 15:53:14 +0900

    [Foldable & dual monitor] feat: visualize WindowMetrics Info

--- FoldablePractice/app/src/main/java/com/pr/foldablepractice/MainActivity.kt
+++ FoldablePractice/app/src/main/java/com/pr/foldablepractice/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.window.layout.WindowInfoTracker
+import androidx.window.layout.WindowMetricsCalculator
 import com.pr.foldablepractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,5 +24,21 @@ class MainActivity : AppCompatActivity() {
         // window manager access
         // 현재 기기의 창 상태의 정보 상태가 담김
         windowInfoTracker = WindowInfoTracker.getOrCreate(this@MainActivity)
+
+        // windowMetrics 정보를 가져오는 메소드
+        obtainWindowMetrics()
+    }
+
+    private fun obtainWindowMetrics() {
+        //  WindowMetricsCalculator 객체를 가져옴
+        val wmc = WindowMetricsCalculator.getOrCreate()
+        // 현재 창의 bound 정보 (Rect 객체)를 반환
+        // 기기 크기에 맞는 Metrics를 가져옴
+        val currentWindowMetrics = wmc.computeCurrentWindowMetrics(this).bounds.flattenToString()
+        val maximumWindowMetrics = wmc.computeMaximumWindowMetrics(this).bounds.flattenToString()
+
+        binding.windowMetrics.text =
+            "CurrentWindowMetrics: ${currentWindowMetrics}\n" +
+            "MaximumWindowMetrics: ${maximumWindowMetrics}"
     }
 }
