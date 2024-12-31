2e4c66f15ea3ee61a3efab3a72d591ffafd7807b
minjong kim <alswhddkdl@gmail.com>
2024-12-31 15:39:18 +0900

    [Foldable & dual monitor] feat: set UI

--- FoldablePractice/app/src/main/java/com/pr/foldablepractice/MainActivity.kt
+++ FoldablePractice/app/src/main/java/com/pr/foldablepractice/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.window.layout.WindowInfoTracker
+import com.pr.foldablepractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     // 윈도우 매니저 변수 선언
     private lateinit var windowInfoTracker: WindowInfoTracker
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // window manager access
         // 현재 기기의 창 상태의 정보 상태가 담김
